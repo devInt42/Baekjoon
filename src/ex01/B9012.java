2e1c76d2e3eb21057f15f07deaaf65241d19a064
unknown <integer402@gmail.com>
2022-10-07 14:13:42 +0900

    1007

--- src/ex01/B9012.java
+++ src/ex01/B9012.java
@@ -1,45 +1,37 @@
 package ex01;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 import java.util.Stack;
-
 public class B9012 {
-
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); //ex) 6
-        boolean flag = true;
-
-        for (int i=0; i<N; i++) {
-            Stack<Character> stack = new Stack<Character>(); //Character형 스택
-            flag = true;
-            String input = sc.next(); //ex) (())))
-            //System.out.println(input);
-
-            for (int j=0; j<input.length(); j++) {
-                char bracket = input.charAt(j); //ex) (
-                //System.out.println(bracket);
-
-                if (bracket == '(') { //열린괄호면
-                    stack.push(bracket); //스택에 담음
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        for(int i=0;i<num;i++){
+            Stack bracket = new Stack();
+            String str = br.readLine();
+            boolean flag = true;
+            for(int j=0; j<str.length();j++) {
+                if (str.charAt(j) == '('){
+                    bracket.push(str.charAt(j));
                 }
-                else { //닫힌괄호면
-                    if (stack.empty()){
+                else {
+                    if(bracket.empty()==true){
                         flag = false;
-                        continue; }//만약 스택이 비어있는 경우에는 꺼낼 수 없으므로 해당 조건문 탈출
-                    else stack.pop(); //스택의 맨 위에 있는 열린괄호를 꺼냄
+                        break;
+                    }
+                    else {
+                        bracket.pop();
+                    }
                 }
-            } //end for j
-
-            if (stack.size()==0 && input.contains(")")==true && flag == true) { //스택이 비었다면 (열린괄호가 모두 꺼내졌다면) 괄호 짝이 맞았다는 뜻
-                System.out.println("YES");
             }
-            else System.out.println("NO");
-
-        } //end for
-
-        sc.close();
+            if(bracket.empty()==false){
+                flag = false;
+            }
+            String res="";
+            res = (flag==true)? "YES" : "NO";
+            System.out.println(res);
+        }
     }
-
 }
--- src/ex01/Main.java
+++ src/ex01/Main.java
@@ -1,13 +1,37 @@
 package ex01;
 
-
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T =sc.nextInt();
-        int N =sc.nextInt();
-
-         }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        for(int i=0;i<num;i++){
+            Stack bracket = new Stack();
+            String str = br.readLine();
+            boolean flag = true;
+            for(int j=0; j<str.length();j++) {
+                if (str.charAt(j) == '('){
+                    bracket.push(str.charAt(j));
+                }
+                else {
+                    if(bracket.empty()==true){
+                        flag = false;
+                        break;
+                    }
+                    else {
+                        bracket.pop();
+                    }
+                }
+            }
+            if(bracket.empty()==false){
+                flag = false;
+            }
+            String res="";
+            res = (flag==true)? "YES" : "NO";
+            System.out.println(res);
+        }
+    }
 }
