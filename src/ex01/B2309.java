bb70608cdff9be1d3ba8984579b491a11fa3655a
unknown <integer402@gmail.com>
2022-10-12 14:12:26 +0900

    1012

---
+++ src/ex01/B2309.java
@@ -0,0 +1,36 @@
+package ex01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B2309 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        int sum = 0;
+        boolean flag = false;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = i + 1; j < 9; j++) {
+                    if ((sum - arr[i] - arr[j]) == 100) {
+                        arr[i] = 0;
+                        arr[j] = 0;
+                        Arrays.sort(arr);
+                        flag=true;
+                        break;
+                    }
+                }
+                if(flag==true) break;
+            }
+
+        for (int i=2; i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
