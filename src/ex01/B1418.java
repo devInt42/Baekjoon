9a2866a41330670923f49d1c08d4858b75ef0044
unknown <integer402@gmail.com>
2022-10-28 16:11:04 +0900

    1028 K-세준 1418번 2

--- src/ex01/B1418.java
+++ src/ex01/B1418.java
@@ -10,8 +10,27 @@ public class B1418 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int K = Integer.parseInt(br.readLine());
+        int res = 0;
 
 
-
+        for (int i = 1; i <= N; i++) {
+            int max = 0;
+            int num = i;
+            for (int j = 2; j <= Math.sqrt(i); ) {
+                if (num % j == 0) {
+                    num /= j;
+                    max = j;
+                } else {
+                    j++;
+                }
+            }
+            if (num != 1) {
+                max = num;
+            }
+            if(max <=K){
+                res++;
+            }
+        }
+        System.out.println(res);
     }
 }
