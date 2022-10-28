dcb2718d678484b32aa6232564fea3013ec91fe9
unknown <integer402@gmail.com>
2022-10-28 14:08:35 +0900

    1028 K-세준수 1418번

--- src/ex01/Main.java
+++ src/ex01/Main.java
@@ -1,24 +1,39 @@
 package ex01;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
 
-        ArrayList list = new ArrayList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        System.out.println(list);
-        list.remove(1);
-        System.out.println(list);
-        System.out.println(list.get(1));
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int arr[] = new int[num - 1];
+        int dasom = sc.nextInt();   //다솜이값
+        if (num == 1) {
+            System.out.println("0");
+            System.exit(0);
+        }
+        int cnt = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) { // 값 저장
+            arr[i] = sc.nextInt();
+        }
+
+        while (true) {
+            Arrays.sort(arr);   //오름차순 정렬
+            max = arr[num - 2];   //max값 저장
+
+            if (dasom > max) {
+                System.out.println(cnt);
+                break;
+            }
+            dasom++;
+            arr[num - 2]--;
+            cnt++;
+
+        } // end while
     }
-
 }
