4db38c53d0455d3812b9f67c7a4f3c69deb445d8
integer <integer402@gmail.com>
2022-11-17 16:23:45 +0900

    1117 백준 1436번 영화감독 숌

--- src/ex01/B1436.java
+++ src/ex01/B1436.java
@@ -1,11 +1,14 @@
 package ex01;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.*;
 
 public class B1436 {
-    public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
         int num = 666;
         int cnt = 1;
