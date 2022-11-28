44a8be85ecc1e1b698ab76bd4c1f52a8dccb7ada
integer <integer402@gmail.com>
2022-11-28 16:03:07 +0900

    1128 11866번 요세푸스 문제 0

--- src/ex01/B11866.java
+++ src/ex01/B11866.java
@@ -10,29 +10,34 @@ import java.util.StringTokenizer;
 public class B11866 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N + 1];
+        int N1 = N;
         ArrayList list = new ArrayList();
         for (int i = 1; i <= N; i++) {
-//            arr[i] = i;
             list.add(i);
         }
-        System.out.print("<");
         int index = K-1;
         int cnt=0;
-        while (cnt<N) {
-            System.out.print(list.get(index));
+        sb.append("<");
+        while (cnt<N1) {
+            if(cnt!=0){
+                sb.append(", ");
+            }
+            sb.append(list.get(index));
             list.remove(index);
-            index += K;
-            if (index>=N-1){
-                index -= N;
+            N-=1;
+            index += K-1;
+            if (N>0 && index>N-1){
+                index %= N;
             }
             cnt++;
         }
-        System.out.print(">");
+        sb.append(">");
+        System.out.println(sb);
 
 
     }
