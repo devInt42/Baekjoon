838be4de88079f8e822f4810a896725cc746af12
integer <integer402@gmail.com>
2022-12-07 18:04:56 +0900

    1207 2032번 반음

--- src/ex01/B2034.java
+++ src/ex01/B2034.java
@@ -12,18 +12,19 @@ public class B2034 {
         int[] arr = new int[n];
         int cnt = 0;
         int res = 0;
+        char[] scale = {'A','X','B','C','X','D','X','E','F','X','G','X'};
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         //65
-        for (int i = 0; i < 11; i++) {     // A~G
-            if (i == 1 || i == 4 || i == 6 || i == 9 || i == 11) {
+        for (int i = 0; i < 12; i++) {     // A~G
+            if (scale[i]=='X') {
                 continue;
             }
             cnt = i;
             res = i;
             for (int j = 0; j < arr.length; j++) {
-                if (res == 1 || res == 4 || res == 6 || res == 9 || res == 11) {
+                if (scale[res]=='X') {
                     break;
                 }
                 res += arr[j];
@@ -31,46 +32,16 @@ public class B2034 {
                     res %= 12;
                 } else if (res < 0) {
                     res = 12 - (Math.abs(res) % 12);
+                    if(res==12){
+                        res=0;
+                    }
                 }
             }
-            if (res == cnt) {
-                char ch = 'K';
-                switch (cnt) {
-                    case 0:
-                        ch = 'A';
-                        break;
-                    case 2:
-                        ch = 'B';
-                        break;
-
-                    case 3:
-                        ch = 'C';
-                        break;
-
-                    case 5:
-                        ch = 'D';
-                        break;
-
-                    case 7:
-                        ch = 'E';
-                        break;
-
-                    case 8:
-                        ch = 'F';
-                        break;
-
-                    case 10:
-                        ch = 'G';
-                        break;
-
-                }
-
-                sb.append(ch + " " + ch + "\n");
-
+            if (scale[cnt]!='X' && scale[res]!='X') {
+                System.out.println(scale[cnt]+" "+scale[res]);
             }
         }
 
-        System.out.println(sb);
 
     }
 }
